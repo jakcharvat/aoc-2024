9672ebdc2d1c9e9376942cf13dcb64e42e2e55d3
Jakub Charvát <jakcharvat@gmail.com>
2026-06-30 13:58:40 +0200

    Update gleam to 1.17.0

--- flake.nix
+++ flake.nix
@@ -4,21 +4,25 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem(system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-      new-day = pkgs.writeScriptBin "new-day" (builtins.readFile ./scripts/new-day.sh);
-      next-day = pkgs.writeScriptBin "next-day" (builtins.readFile ./scripts/next-day.sh);
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        new-day = pkgs.writeScriptBin "new-day" (builtins.readFile ./scripts/new-day.sh);
+        next-day = pkgs.writeScriptBin "next-day" (builtins.readFile ./scripts/next-day.sh);
 
-    in {
-      devShell = pkgs.mkShell {
-        packages = with pkgs; [
-          gleam
-          erlang_26
-          new-day
-          next-day
-        ];
-      };
-    }
-  );
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            gleam
+            beamPackages.erlang
+            new-day
+            next-day
+          ];
+        };
+      }
+    );
 }
